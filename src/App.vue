76bcea9c5ecce2f578fc3e2b4a0a6c26433e4abc
taufardh <ardhwood@gmail.com>
2020-01-28 11:07:59 +0700

    add function and fix damn BUG

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <div id="app">
     <div class="container">
-      <Todos />
+      <AddTodo/>
+      <FilterTodos/>
+      <Todos/>
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
+import FilterTodos from "./components/FilterTodos.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Todos
+    Todos,
+    AddTodo,
+    FilterTodos
   }
-}
+};
 </script>
 
 <style>
 body {
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
-  background: #eBf7f0;
+  background: #e8f7f0;
 }
 .container {
   max-width: 1100px;
@@ -29,5 +35,4 @@ body {
   overflow: auto;
   padding: 0 2rem;
 }
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import todos from './modules/todos';
 
-//Load Vuex
+// Load Vuex
 Vue.use(Vuex);
 
-//Create store
+// Create store
 export default new Vuex.Store({
     modules: {
         todos
